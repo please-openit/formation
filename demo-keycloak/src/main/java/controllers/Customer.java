072790e3fb5122485809e4f8dc8b159453324667
Mathieu Passenaud <mathieu.passenaud@please-open.it>
2020-08-25 09:29:00 +0200

    fix

--- demo-keycloak/src/main/java/controllers/Customer.java
+++ demo-keycloak/src/main/java/controllers/Customer.java
@@ -1,19 +1,17 @@
 package controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
-@Controller("customers")
+@RestController
+@RequestMapping("/customers")
 public class Customer {
-    @GetMapping(path = "/")
-    public String index() {
-        return "external";
-    }
 
-    @GetMapping(path = "/customers")
+    @GetMapping(path = "/")
     public String customers(Principal principal, Model model) {
         return "customers";
     }
